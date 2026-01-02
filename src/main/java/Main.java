6cfb1739bc92721c4cd744f8180ab274b151fc48
kbs <kim3210542@gmail.com>
2026-01-02 20:53:54 +0900

    3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,10 +2,12 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.print("$ ");
+        while (true) {
+            System.out.print("$ ");
 
-        Scanner sc = new Scanner(System.in);
-        String command = sc.nextLine();
-        System.out.println(command + ": command not found");
+            Scanner sc = new Scanner(System.in);
+            String command = sc.nextLine();
+            System.out.println(command + ": command not found");
+        }
     }
 }
