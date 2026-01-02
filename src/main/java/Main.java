f5667a976a25d3f622ef1061730fcf4ef8af6e62
kbs <kim3210542@gmail.com>
2026-01-02 20:45:28 +0900

    2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
         String command = sc.nextLine();
+        System.out.println("$ ");
         System.out.println(command + ": command not found");
     }
 }
