205fad276626bbb353fff7a33dbc32ef292dbc9e
kbs <kim3210542@gmail.com>
2026-01-04 04:28:06 +0900

    8

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,13 +12,36 @@ public class Main {
                 String input = sc.nextLine();
                 String[] parts = input.split(" ",2);
                 String command = parts[0];
-                switch (command) {
-                    case "type" -> typeCheck(parts.length>1 ? parts[1] : "");
-                    case "exit" -> System.exit(0);
-                    case "echo" -> System.out.println(parts[1]);
-                    default -> System.out.println(command + ": command not found");
-                }
+                if (BuiltinCommand.from(command)!=null) handleBuiltin(command, parts);
+                else handleExternal(command, parts);
             }
+        }
+    }
+
+    private static void handleExternal(String command, String[] args) {
+        String fullPath = getFullPath(command);
+
+        if (fullPath != null){
+            try {
+                ProcessBuilder pb = new ProcessBuilder(args);
+                pb.inheritIO();
+                Process process = pb.start();
+                process.waitFor();
+            } catch (Exception e) {
+                System.err.println(e.getMessage());
+            }
+        } else {
+            System.out.println(command + ": command not found");
+        }
+
+    }
+
+    private static void handleBuiltin(String command, String[] parts) {
+        switch (command) {
+            case "type" -> typeCheck(parts.length>1 ? parts[1] : "");
+            case "exit" -> System.exit(0);
+            case "echo" -> System.out.println(parts[1]);
+            default -> System.out.println(command + ": command not found");
         }
     }
 
@@ -27,19 +50,28 @@ public class Main {
            System.out.println(command + " is a shell builtin");
            return;
         }
+        String fullPath = getFullPath(command);
+        if (fullPath != null){
+            System.out.println(command + " is " + fullPath);
+        } else{
+            System.out.println(command + ": not found");
+        }
+    }
+
+    private static String getFullPath(String command) {
         String pathEnv = System.getenv("PATH");
         if (pathEnv != null) {
             String[] paths = pathEnv.split(File.pathSeparator);
             for (String path : paths) {
                 Path fullPath = Paths.get(path, command);
                 if (Files.exists(fullPath) && Files.isExecutable(fullPath)) {
-                    System.out.println(command + " is " + fullPath);
-                    return;
+                    return fullPath.toString();
                 }
             }
         }
-        System.out.println(command + ": not found");
+        return null;
     }
+
     private enum BuiltinCommand {
         ECHO("echo"),
         EXIT("exit"),
