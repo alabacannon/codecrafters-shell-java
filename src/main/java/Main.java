1044018b1966513840902796f6e084e552855be3
kbs <kim3210542@gmail.com>
2026-01-04 01:30:45 +0900

    5

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,14 +2,22 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        while (true) {
+        try(Scanner sc = new Scanner(System.in)){
             System.out.print("$ ");
+            while (sc.hasNextLine()) {
 
-            Scanner sc = new Scanner(System.in);
-            String command = sc.nextLine();
-            if (command.equals("exit")) break;
+                String input = sc.nextLine();
+                String command = input.split(" ")[0];
+                switch (command) {
+                    case "exit" -> System.exit(0);
+                    case "echo" -> System.out.println(input.split(" ",2)[1]);
+                    default -> System.out.println(command + ": command not found");
+                }
 
-            System.out.println(command + ": command not found");
+                System.out.print("$ ");
+
+            }
+
         }
     }
 }
