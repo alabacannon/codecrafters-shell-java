f01c780fd9666ae2c67aba099ddd6e809bb60170
kbs <kim3210542@gmail.com>
2026-01-04 04:31:34 +0900

    8

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,10 +10,11 @@ public class Main {
             while (true) {
                 System.out.print("$ ");
                 String input = sc.nextLine();
-                String[] parts = input.split(" ");
+                String[] parts = input.split(" ",2);
+                String[] arguments = input.split(" ");
                 String command = parts[0];
                 if (BuiltinCommand.from(command)!=null) handleBuiltin(command, parts);
-                else handleExternal(command, parts);
+                else handleExternal(command, arguments);
             }
         }
     }
